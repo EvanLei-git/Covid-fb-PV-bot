4af69c2e0e2913946992202918119fe589305d65
Zeto-code <71707767+Zeto-code@users.noreply.github.com>
2020-10-12 13:42:19 +0300

    Update send_to.py

--- send_to.py
+++ send_to.py
@@ -1,4 +1,5 @@
 #write the profile id(most likely it only has numbers)
 #of the people you want to notify
-mom="**********"    
-dad="**********"
+#example
+mom="9581085520525"    
+dad="5236738129246"
